a265b189c834896fc269789392e3977f8a1273c2
Josh Hawks <snl95@txstate.edu>
2022-09-30 08:39:54 -0500

    schema updated

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS 'employees';
+
+CREATE DATABASE 'employees';
+
+USE 'employees';
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  name VARCHAR(30) NOT NULL,
+)
+
+CREATE TABLE roles (
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(department_id)
+)
+
+CREATE TABLE employees (
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(role_id),
+  FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+)
